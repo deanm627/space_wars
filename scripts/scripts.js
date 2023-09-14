0446605a11b01f5d65804b0d49e23fb31a2c9f68
Mary Dean <mdean726@gmail.com>
2023-09-14 14:24:52 -0400

    Updates with ppl list

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     
     const starWarsPerson = document.getElementById('starWarsPerson');
+    let peopleList = document.createElement('ul');
 
     fetch('https://swapi.dev/api/people/1/')
         .then(function(response) {
@@ -10,6 +11,22 @@ document.addEventListener('DOMContentLoaded', function () {
         })
         .then(function(data) {
             console.log(data);
-                starWarsPerson.innerText = data.name;
+            starWarsPerson.innerText = data.name;
+        });
+
+    // This returns all the names in a list
+    //
+    fetch('https://swapi.dev/api/people/')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(data);
+            for (let i=0; i<data.results.length; i++) {
+                let listItem = document.createElement('li');
+                listItem.innerText = data.results[i].name;
+                peopleList.append(listItem);
+            }
+            starWarsPerson.appendChild(peopleList);
         });
 });

89a739b08bdd6c10921199e7752d66229d72a113
Mary Dean <mdean726@gmail.com>
2023-09-18 00:02:33 -0400

    Updated with dropdown details lists and styling, not reseting details after selecting new item

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,12 @@ const peopleList = document.getElementById('peopleList');
 const planetList = document.getElementById('planetList');
 const starshipList = document.getElementById('starshipList');
 const result = document.getElementById('result');
+const peopleListDetails = document.getElementById('peopleListDetails');
+const planetListDetails = document.getElementById('planetListDetails');
 const starshipListDetails = document.getElementById('starshipListDetails');
+const firstList = document.querySelectorAll('.firstList');
+const secondList = document.querySelectorAll('.secondList');
+
 
 document.addEventListener('DOMContentLoaded', function() {
     console.log("DOM loaded");
@@ -14,30 +19,59 @@ document.addEventListener('DOMContentLoaded', function() {
         makeList(response, peopleList);
     });
 
-    peopleList.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const selectedItemUrl = peopleList.querySelector('select').value;
-        get(selectedItemUrl).then(function(response) {
-            displayResult(response);
-        });
-    });
-
     const planetUrl = "https://swapi.dev/api/planets/";
     get(planetUrl).then(function(response) {
         makeList(response, planetList);
     });
 
+    const starshipUrl = "https://swapi.dev/api/starships/";
+    get(starshipUrl).then(function(response) {
+        makeList(response, starshipList);
+    });
+
+    let selectedPeopleObj = "";
+    peopleList.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const selectedItemUrl = peopleList.querySelector('select').value;
+        get(selectedItemUrl).then(function(response) {
+            selectedPeopleObj = response;
+            displayOptionsList(response, peopleListDetails);
+        });
+    });
+
+    peopleListDetails.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const selectedPeopleDetail = peopleListDetails.querySelector('select').value;
+        const selectedPeopleObjArr = Object.entries(selectedPeopleObj);
+        const itemToDisplay = document.getElementById('itemToDisplay');
+        for (let i=0; i<selectedPeopleObjArr.length; i++) {
+            if (selectedPeopleObjArr[i][0] === selectedPeopleDetail) {
+                itemToDisplay.innerText = selectedPeopleObjArr[i][1];
+            };
+        };
+    });
+
+    let selectedPlanetObj = "";
     planetList.addEventListener('submit', function(event) {
         event.preventDefault();
         const selectedItemUrl = planetList.querySelector('select').value;
         get(selectedItemUrl).then(function(response) {
-            displayResult(response);
+            selectedPlanetObj = response;
+
+            displayOptionsList(response, planetListDetails);
         });
     });
 
-    const starshipUrl = "https://swapi.dev/api/starships/";
-    get(starshipUrl).then(function(response) {
-        makeList(response, starshipList);
+    planetListDetails.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const selectedPlanetDetail = planetListDetails.querySelector('select').value;
+        const selectedPlanetObjArr = Object.entries(selectedPlanetObj);
+        const itemToDisplay = document.getElementById('itemToDisplay');
+        for (let i=0; i<selectedPlanetObjArr.length; i++) {
+            if (selectedPlanetObjArr[i][0] === selectedPlanetDetail) {
+                itemToDisplay.innerText = selectedPlanetObjArr[i][1];
+            };
+        };
     });
 
     let selectedStarshipObj = "";
@@ -46,7 +80,6 @@ document.addEventListener('DOMContentLoaded', function() {
         const selectedItemUrl = starshipList.querySelector('select').value;
         get(selectedItemUrl).then(function(response) {
             selectedStarshipObj = response;
-            // displayResult(response);
             displayOptionsList(response, starshipListDetails);
         });
     });
@@ -55,52 +88,22 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
         const selectedStarshipDetail = starshipListDetails.querySelector('select').value;
         const selectedStarshipObjArr = Object.entries(selectedStarshipObj);
-        let itemToDisplay = document.createElement('p');
+        const itemToDisplay = document.getElementById('itemToDisplay');
         for (let i=0; i<selectedStarshipObjArr.length; i++) {
             if (selectedStarshipObjArr[i][0] === selectedStarshipDetail) {
-                itemToDisplay = selectedStarshipObjArr[i][1];
-            }
-        }
-        result.append(itemToDisplay);
+                itemToDisplay.innerText = selectedStarshipObjArr[i][1];
+            };
+        };
     });
 
 });
 
 function makeList(obj, category) {
-    let selectList = document.createElement('select');
+    const selectList = document.createElement('select');
     for (let i=0; i<obj.results.length; i++) {
-        let listItem = document.createElement('option');
+        const listItem = document.createElement('option');
         listItem.value = obj.results[i].url;
         listItem.text = obj.results[i].name;
-        selectList.appendChild(listItem);
-    };
-    category.append(selectList);
-};
-
-// function displayResult(obj) {
-//     const resultArr = Object.entries(obj);
-//     const objList = document.createElement('ul');
-//     for (let i=0; i<resultArr.length; i++) {
-//         let objProp = document.createElement('li');
-//         objProp.textContent = resultArr[i][0] + ": "
-//         for (let j=1; j<resultArr[i].length; j++) {
-//             objProp.textContent = objProp.textContent + " " + resultArr[i][j] + " ";
-//         };
-//         objList.appendChild(objProp);
-//     };
-//     result.append(objList);
-// };
-
-function displayOptionsList(obj, category) {
-    console.log(obj);
-    const keyListArr = Object.keys(obj);
-    console.log(keyListArr);
-    const filteredArr = keyListArr.filter((key) => key !== "name" && key !== "created" && key !== "edited" && key !== "url");
-    let selectList = document.createElement('select');
-    for (let i=0; i<filteredArr.length; i++) {
-        let listItem = document.createElement('option');
-        listItem.value = filteredArr[i];
-        listItem.text = filteredArr[i];
         selectList.appendChild(listItem);
     };
     category.append(selectList);
@@ -108,4 +111,23 @@ function displayOptionsList(obj, category) {
     button.type = "submit";
     button.textContent = "Submit";
     category.append(button);
+    return;
+};
+
+function displayOptionsList(obj, category) {
+    const keyListArr = Object.keys(obj);
+    const filteredArr = keyListArr.filter((key) => key !== "name" && key !== "created" && key !== "edited" && key !== "url" && key !== "model");
+    const list = document.createElement('select');
+    for (let i=0; i<filteredArr.length; i++) {
+        const listItem = document.createElement('option');
+        listItem.value = filteredArr[i];
+        listItem.text = filteredArr[i];
+        list.appendChild(listItem);
+    };
+    category.append(list);
+    const button = document.createElement('button');
+    button.type = "submit";
+    button.textContent = "Submit";
+    category.append(button);
+    return;
 };
